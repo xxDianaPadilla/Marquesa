ee2601747b8025568cf97f1607be25b4274b5303
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-06-04 14:57:31 -0600

    Terminé el diseño de la página de verificación de código

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import RecoverPassword from './pages/RecoverPassword';
+import VerificationCode from './pages/VerificationCode';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <RecoverPassword/>
+    <VerificationCode/>
   )
 }
 

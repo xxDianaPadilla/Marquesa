66aa0309d0c152f4f6ca6c28dcd9cb41d9bccb9a
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-06-30 09:13:41 -0600

    Arreglé el diseño de las páginas de login, registro, y recuperación de contraseña

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,13 @@ import VerificationCode from './pages/VerificationCode';
 import UpdatePassword from './pages/UpdatePassword';
 import Dashboard from './pages/Dashboard';
 import Saves from './pages/Saves';
-import Header from './components/Header/Header';
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Saves/>
+    <Register/>
   )
 }
 

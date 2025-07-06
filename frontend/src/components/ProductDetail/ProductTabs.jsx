23381571d537b4cdc48c85c43f29d4ed99285d20
Jaim3w <jaimeperla26@gmail.com>
2025-07-05 21:05:31 -0600

    cambios en resposive de emergencia

--- frontend/src/components/ProductDetail/ProductTabs.jsx
+++ frontend/src/components/ProductDetail/ProductTabs.jsx
@@ -8,14 +8,14 @@ const ProductTabs = ({ tab, setTab, product }) => {
   };
 
   return (
-    <div className="border-t border-gray-200 mt-6 pt-4">
-      <div className="flex gap-2 bg-gray-100 rounded-md text-sm font-medium overflow-hidden">
+    <div className="border-t border-gray-200 mt-6 pt-4 ">
+      <div className="flex gap-2 bg-gray-100 rounded-md text-sm font-medium overflow-hidden cursor-pointer">
         {['description', 'details', 'shipping'].map((key) => (
           <button
             key={key}
             onClick={() => setTab(key)}
             className={`flex-1 px-4 py-2 text-center ${
-              tab === key ? 'bg-white text-black font-semibold shadow-sm' : 'text-gray-500 hover:text-[#CD5277]'
+              tab === key ? 'bg-white text-black font-semibold shadow-sm' : 'text-gray-500 hover:text-[#CD5277] cursor-pointer'
             }`}
           >
             {key === 'description' ? 'Descripción' : key === 'details' ? 'Detalles' : 'Envío'}

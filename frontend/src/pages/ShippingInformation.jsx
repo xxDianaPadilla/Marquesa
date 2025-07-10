afc70d3d164f2231c35287fe9c59dc02a94cca53
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-07-10 12:01:19 -0600

    Página de información de envíos terminada

--- frontend/src/pages/ShippingInformation.jsx
+++ frontend/src/pages/ShippingInformation.jsx
@@ -27,11 +27,57 @@ const ShippingInformation = () => {
                             En La MarquesaSV ofrecemos servicio de entrega a domicilio en determinadas zonas de El Salvador. A continuación, los detalles clave:
                         </p>
                         <br />
-                        <div className="text-justify mb-8" style={{ marginLeft: '110px' }}>
+                        <div className="text-justify mb-8" style={{ marginLeft: '90px' }}>
+                            <ol>
+                                <li>1. Horarios de entrega</li>
+                            </ol>
+                        </div>
+                        <div className="text-justify mb-8" style={{ marginLeft: '150px' }}>
                             <ul style={{ listStyle: 'inherit' }}>
-                                <li>Horarios de entrega</li>
+                                <li>Lunes a sábado: 9:00 a.m. – 5:00 p.m.</li>
+                                <li>No se realizan entregas los domingos ni feriados (salvo fechas especiales anunciadas).</li>
                             </ul>
                         </div>
+
+                        <div className="text-justify mb-8" style={{ marginLeft: '90px' }}>
+                            <ol>
+                                <li>2. Tiempo de entrega</li>
+                            </ol>
+                        </div>
+                        <div className="text-justify mb-8" style={{ marginLeft: '150px' }}>
+                            <ul style={{ listStyle: 'inherit' }}>
+                                <li>Se recomienda hacer el pedido con al menos 24 a 48 horas de anticipación.</li>
+                                <li>Entregas urgentes pueden estar sujetas a disponibilidad con un cargo adicional</li>
+                            </ul>
+                        </div>
+
+                        <div className="text-justify mb-8" style={{ marginLeft: '90px' }}>
+                            <ol>
+                                <li>3. Cobertura</li>
+                            </ol>
+                        </div>
+                        <div className="text-justify mb-8" style={{ marginLeft: '150px' }}>
+                            <ul style={{ listStyle: 'inherit' }}>
+                                <li>Realizamos entregas en áreas seleccionadas dentro del área metropolitana de San Salvador. Consultar con antelación si la zona es cubierta.</li>
+                            </ul>
+                        </div>
+
+                        <div className="text-justify mb-8" style={{ marginLeft: '90px' }}>
+                            <ol>
+                                <li>4. Información requerida</li>
+                                <br />
+                                <p>El cliente debe proporcionar:</p>
+                            </ol>
+                        </div>
+                        <div className="text-justify mb-8" style={{ marginLeft: '150px' }}>
+                            <ul style={{ listStyle: 'inherit' }}>
+                                <li>Dirección completa y exacta</li>
+                                <li>Nombre del destinatario</li>
+                                <li>Número de contacto activo</li>
+                            </ul>
+                        </div>
+
+                        <p>La MarquesaSV no se hace responsable por retrasos o entregas fallidas debido a información incorrecta o ausencia del destinatario.</p>
                     </div>
                 </main>
             </div>

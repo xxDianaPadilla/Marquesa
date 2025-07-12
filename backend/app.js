f6d22f25d5584758a73766edeb358a2d1668ff18
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-07-11 19:09:11 -0600

    Borrando funcionalidades redundantes de la app.js

--- backend/app.js
+++ backend/app.js
@@ -49,8 +49,6 @@ app.use(
    cors({
        origin: "http://localhost:5173",
        credentials: true,
-       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-       allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
    })
 );
 

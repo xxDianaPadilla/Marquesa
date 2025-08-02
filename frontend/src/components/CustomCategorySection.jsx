1ea22b43ce5252ab8b17c99130ca046637c1402a
xxDianaPadilla <dianagabypadilla006@gmail.com>
2025-08-02 13:13:21 -0600

    Eliminé el icono de seleccionado de las cards de los materiales para personalización ya que se sobreponia en el formulario de confirmación de personalización

--- frontend/src/components/CustomCategorySection.jsx
+++ frontend/src/components/CustomCategorySection.jsx
@@ -107,16 +107,6 @@ const ProductCard = ({
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
         >
-            {/* Badge de seleccionado - Z-INDEX REDUCIDO */}
-            {isSelected && (
-                <div className="absolute top-2 right-2 z-10">
-                    <div className="bg-pink-500 text-white rounded-full p-1">
-                        <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                            <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-                </div>
-            )}
 
             {/* Badge de sin stock - Z-INDEX REDUCIDO */}
             {!product.inStock && (
@@ -190,7 +180,7 @@ const ProductCard = ({
                     onClick={product.inStock ? onCustomize : undefined}
                     disabled={!product.inStock}
                     className={buttonClasses}
-                    style={{cursor: 'pointer'}}
+                    style={{ cursor: 'pointer' }}
                 >
                     {isSelected ? (
                         <>
